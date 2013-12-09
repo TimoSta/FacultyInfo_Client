016c7c7b9161a361f77025c19aafa3c7f305a5dd
Daniel <Daniel@192.168.0.13>
2013-12-09 15:34:12 +0100

    ActionBar, DropDownMenü, Bearbeiten-Button eingestellt.

--- FacultyInfo_App/src/de/uni_passau/facultyinfo/client/MainActivity.java
+++ FacultyInfo_App/src/de/uni_passau/facultyinfo/client/MainActivity.java
@@ -1,15 +1,27 @@
 package de.uni_passau.facultyinfo.client;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		getActionBar().setDisplayHomeAsUpEnabled(false);
+		getActionBar().setDisplayShowHomeEnabled(false);
+		getActionBar().setDisplayShowTitleEnabled(false);
+		
+		
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+				R.array.spinner_menu, android.R.layout.simple_spinner_item);
+		getActionBar().setListNavigationCallbacks(adapter, null);
+		
 	}
 
 	@Override
