45102aed8d230a145e8cca461aa68a8e3fbcb1e4
Daniel <Daniel@132.231.214.171>
2014-01-07 14:57:38 +0100

    Maps implementiert

--- FacultyInfo_App/src/de/uni_passau/facultyinfo/client/activity/MainActivity.java
+++ FacultyInfo_App/src/de/uni_passau/facultyinfo/client/activity/MainActivity.java
@@ -20,6 +20,7 @@ import de.uni_passau.facultyinfo.client.fragment.BuslinesFragment;
 import de.uni_passau.facultyinfo.client.fragment.CafeteriaFragment;
 import de.uni_passau.facultyinfo.client.fragment.ContactFragment;
 import de.uni_passau.facultyinfo.client.fragment.FaqsFragment;
+import de.uni_passau.facultyinfo.client.fragment.MapFragment;
 import de.uni_passau.facultyinfo.client.fragment.NewsFragment;
 import de.uni_passau.facultyinfo.client.fragment.SportsFragment;
 import de.uni_passau.facultyinfo.client.fragment.TimetableFragment;
@@ -49,90 +50,90 @@ public class MainActivity extends Activity {
 		mDrawerList.setAdapter(new ArrayAdapter<String>(this,
 				R.layout.drawer_list_item, drawerValues));
 		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
-		
+
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-        getActionBar().setHomeButtonEnabled(true);
-		
-		//mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(
-                this,                  /* host Activity */
-                mDrawerLayout,         /* DrawerLayout object */
-                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
-                R.string.drawer_open,  /* "open drawer" description */
-                R.string.drawer_close  /* "close drawer" description */
-                ) {
+		getActionBar().setHomeButtonEnabled(true);
 
-            /** Called when a drawer has settled in a completely closed state. */
-            public void onDrawerClosed(View view) {
-                getActionBar().setTitle(mTitle);
-            }
+		// mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+		mDrawerToggle = new ActionBarDrawerToggle(this, /* host Activity */
+		mDrawerLayout, /* DrawerLayout object */
+		R.drawable.ic_drawer, /* nav drawer icon to replace 'Up' caret */
+		R.string.drawer_open, /* "open drawer" description */
+		R.string.drawer_close /* "close drawer" description */
+		) {
 
-            /** Called when a drawer has settled in a completely open state. */
-            public void onDrawerOpened(View drawerView) {
-                getActionBar().setTitle(mDrawerTitle);
-                invalidateOptionsMenu(); 
-            }
-        };
+			/** Called when a drawer has settled in a completely closed state. */
+			public void onDrawerClosed(View view) {
+				getActionBar().setTitle(mTitle);
+			}
 
-        // Set the drawer toggle as the DrawerListener
-        mDrawerLayout.setDrawerListener(mDrawerToggle);
-        
-//        if (savedInstanceState == null) {
-//            selectItem(0);
-//        }
+			/** Called when a drawer has settled in a completely open state. */
+			public void onDrawerOpened(View drawerView) {
+				getActionBar().setTitle(mDrawerTitle);
+				invalidateOptionsMenu();
+			}
+		};
 
-    }
-	
+		// Set the drawer toggle as the DrawerListener
+		mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+		// if (savedInstanceState == null) {
+		// selectItem(0);
+		// }
+
+	}
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-	
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        mDrawerToggle.syncState();
-    }
+	@Override
+	public boolean onPrepareOptionsMenu(Menu menu) {
+		// If the nav drawer is open, hide action items related to the content
+		// view
+		boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+		menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
+		return super.onPrepareOptionsMenu(menu);
+	}
 
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
-    }
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		// Sync the toggle state after onRestoreInstanceState has occurred.
+		mDrawerToggle.syncState();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(android.view.MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-          return true;
-        }
-        // Handle your other action bar items...
+	@Override
+	public void onConfigurationChanged(Configuration newConfig) {
+		super.onConfigurationChanged(newConfig);
+		mDrawerToggle.onConfigurationChanged(newConfig);
+	}
 
-        return super.onOptionsItemSelected(item);
-    }
+	@Override
+	public boolean onOptionsItemSelected(android.view.MenuItem item) {
+		// Pass the event to ActionBarDrawerToggle, if it returns
+		// true, then it has handled the app icon touch event
+		if (mDrawerToggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		// Handle your other action bar items...
 
-		// getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-		// getActionBar().setDisplayHomeAsUpEnabled(false);
-		// getActionBar().setDisplayShowHomeEnabled(true);
-		// getActionBar().setDisplayShowTitleEnabled(false);
-		//
-		// ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-		// this, R.array.spinner_menu,
-		// android.R.layout.simple_spinner_item);
-		// getActionBar().setListNavigationCallbacks(adapter, this);
-	//}
+		return super.onOptionsItemSelected(item);
+	}
+
+	// getActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+	// getActionBar().setDisplayHomeAsUpEnabled(false);
+	// getActionBar().setDisplayShowHomeEnabled(true);
+	// getActionBar().setDisplayShowTitleEnabled(false);
+	//
+	// ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+	// this, R.array.spinner_menu,
+	// android.R.layout.simple_spinner_item);
+	// getActionBar().setListNavigationCallbacks(adapter, this);
+	// }
 
 	@Override
 	protected void onResume() {
@@ -145,12 +146,12 @@ public class MainActivity extends Activity {
 
 	}
 
-//	@Override
-//	public boolean onCreateOptionsMenu(Menu menu) {
-//		// Inflate the menu; this adds items to the action bar if it is present.
-//		getMenuInflater().inflate(R.menu.main, menu);
-//		return true;
-//	}
+	// @Override
+	// public boolean onCreateOptionsMenu(Menu menu) {
+	// // Inflate the menu; this adds items to the action bar if it is present.
+	// getMenuInflater().inflate(R.menu.main, menu);
+	// return true;
+	// }
 
 	/* The click listner for ListView in the navigation drawer */
 	private class DrawerItemClickListener implements
@@ -163,40 +164,42 @@ public class MainActivity extends Activity {
 	}
 
 	private void selectItem(int position) {
-		Fragment fragment=null; 
-		
-		
+		Fragment fragment = null;
+
 		// update the main content by replacing fragments
-		switch (position){
-		case 0: 
-			System.out.println("home"); 
-			break; 
-		case 1: 
-			System.out.println("news"); 
-			fragment = new NewsFragment(); 
-			break; 
-		case 2: 
-			System.out.println("timetable"); 
-			fragment = new TimetableFragment(); 
-			break; 
-		case 3: 
-			System.out.println("busfahrplan"); 
-			fragment = new BuslinesFragment(); 
-			break; 
-		case 4: 
-			fragment = new SportsFragment(); 
-			break; 
-		case 5: 
-			fragment = new CafeteriaFragment(); 
-			break; 
-		case 6: 
-			fragment = new ContactFragment(); 
-			break; 
-		case 7: 
-			fragment = new FaqsFragment(); 
-			break; 
+		switch (position) {
+		case 0:
+			System.out.println("home");
+			break;
+		case 1:
+			System.out.println("news");
+			fragment = new NewsFragment();
+			break;
+		case 2:
+			System.out.println("timetable");
+			fragment = new TimetableFragment();
+			break;
+		case 3:
+			System.out.println("busfahrplan");
+			fragment = new BuslinesFragment();
+			break;
+		case 4:
+			fragment = new SportsFragment();
+			break;
+		case 5:
+			fragment = new CafeteriaFragment();
+			break;
+		case 6:
+			fragment = new ContactFragment();
+			break;
+		case 7:
+			fragment = new FaqsFragment();
+			break;
+		case 8:
+			fragment = new MapFragment();
+			break;
 		}
-		
+
 		FragmentManager fragmentManager = getFragmentManager();
 		fragmentManager.beginTransaction()
 				.replace(R.id.content_frame, fragment).commit();
@@ -206,12 +209,12 @@ public class MainActivity extends Activity {
 		setTitle(drawerValues[position]);
 		mDrawerLayout.closeDrawer(mDrawerList);
 	}
-	
-	public void showDay(View view){
-		System.out.println(view.getId()); 
-		int viewId = view.getId(); 
-		int dayId=0; 
-		
+
+	public void showDay(View view) {
+		System.out.println(view.getId());
+		int viewId = view.getId();
+		int dayId = 0;
+
 		if (viewId == (findViewById(R.id.mo).getId())
 				|| viewId == (findViewById(R.id.mo810).getId())
 				|| viewId == (findViewById(R.id.mo1012).getId())
@@ -219,60 +222,51 @@ public class MainActivity extends Activity {
 				|| viewId == (findViewById(R.id.mo1416).getId())
 				|| viewId == (findViewById(R.id.mo1618).getId())
 				|| viewId == (findViewById(R.id.mo1820).getId())) {
-			dayId=0; 
-		}else if (viewId == (findViewById(R.id.di).getId())
+			dayId = 0;
+		} else if (viewId == (findViewById(R.id.di).getId())
 				|| viewId == (findViewById(R.id.di810).getId())
 				|| viewId == (findViewById(R.id.di1012).getId())
 				|| viewId == (findViewById(R.id.di1214).getId())
 				|| viewId == (findViewById(R.id.di1416).getId())
 				|| viewId == (findViewById(R.id.di1618).getId())
 				|| viewId == (findViewById(R.id.di1820).getId())) {
-			dayId=1; 
-		}else if (viewId == (findViewById(R.id.mi).getId())
+			dayId = 1;
+		} else if (viewId == (findViewById(R.id.mi).getId())
 				|| viewId == (findViewById(R.id.mi810).getId())
 				|| viewId == (findViewById(R.id.mi1012).getId())
 				|| viewId == (findViewById(R.id.mi1214).getId())
 				|| viewId == (findViewById(R.id.mi1416).getId())
 				|| viewId == (findViewById(R.id.mi1618).getId())
 				|| viewId == (findViewById(R.id.mi1820).getId())) {
-			dayId=2; 
-		}else if (viewId == (findViewById(R.id.don).getId())
+			dayId = 2;
+		} else if (viewId == (findViewById(R.id.don).getId())
 				|| viewId == (findViewById(R.id.don810).getId())
 				|| viewId == (findViewById(R.id.don1012).getId())
 				|| viewId == (findViewById(R.id.don1214).getId())
 				|| viewId == (findViewById(R.id.don1416).getId())
 				|| viewId == (findViewById(R.id.don1618).getId())
 				|| viewId == (findViewById(R.id.don1820).getId())) {
-			dayId=3; 
-		}else if (viewId == (findViewById(R.id.fr).getId())
+			dayId = 3;
+		} else if (viewId == (findViewById(R.id.fr).getId())
 				|| viewId == (findViewById(R.id.fr810).getId())
 				|| viewId == (findViewById(R.id.fr1012).getId())
 				|| viewId == (findViewById(R.id.fr1214).getId())
 				|| viewId == (findViewById(R.id.fr1416).getId())
 				|| viewId == (findViewById(R.id.fr1618).getId())
 				|| viewId == (findViewById(R.id.fr1820).getId())) {
-			dayId=4;  
+			dayId = 4;
 		}
-		
-		Intent intent = new Intent (this, DisplayDayActivity.class); 
-		
-		intent.putExtra("dayId", dayId); 
-		startActivity(intent); 
-		System.out.println("startActivity"); 
+
+		Intent intent = new Intent(this, DisplayDayActivity.class);
+
+		intent.putExtra("dayId", dayId);
+		startActivity(intent);
+		System.out.println("startActivity");
 	}
-	
 
 	/**
 	 * Fragment that appears in the "content_frame", shows a planet
 	 */
-	
-
-
-	
-
-	
-	
-	
 
 	// @Override
 	// public boolean onNavigationItemSelected(int itemPosition, long itemId) {
