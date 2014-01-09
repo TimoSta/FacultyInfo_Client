c5991fc6ee5c66e559477855c7ea469b3a2bb7bf
Luisa <Luisa@132.231.178.39>
2014-01-09 12:34:02 +0100

    Ansprechpartner

--- FacultyInfo_App/src/de/uni_passau/facultyinfo/client/activity/DisplayChairContactsActivity.java
+++ FacultyInfo_App/src/de/uni_passau/facultyinfo/client/activity/DisplayChairContactsActivity.java
@@ -1,22 +1,35 @@
 package de.uni_passau.facultyinfo.client.activity;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import de.uni_passau.facultyinfo.client.R;
 import de.uni_passau.facultyinfo.client.R.id;
 import de.uni_passau.facultyinfo.client.R.layout;
 import de.uni_passau.facultyinfo.client.R.menu;
+import de.uni_passau.facultyinfo.client.model.access.AccessFacade;
+import de.uni_passau.facultyinfo.client.model.dto.ContactGroup;
+import de.uni_passau.facultyinfo.client.model.dto.ContactPerson;
+import de.uni_passau.facultyinfo.client.util.AsyncDataLoader;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class DisplayChairContactsActivity extends Activity {
+	private String chairId; 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +37,25 @@ public class DisplayChairContactsActivity extends Activity {
 		setContentView(R.layout.activity_display_chair_contacts);
 		
 		Intent intent = getIntent(); 
-		String chairId = intent.getStringExtra("chairId"); 
+		chairId = intent.getStringExtra("chairId"); 
 		
-		ListView listView = (ListView)findViewById(R.id.chairContacts);
+//		ListView listView = (ListView)findViewById(R.id.chairContacts);
+//		
+//		List valueList = new ArrayList<String>();
+//
+//		for (int i = 0; i < 5; i++) {
+//			valueList.add("Lehrstuhl: " + chairId + " Kontakt " + i); 
+//		}
+//		
+//		ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+//		
+//		listView.setAdapter(adapter); 
 		
-		List valueList = new ArrayList<String>();
-
-		for (int i = 0; i < 5; i++) {
-			valueList.add("Lehrstuhl: " + chairId + " Kontakt " + i); 
-		}
+		ContactLoader contactLoader = new ContactLoader(getCurrentFocus()); 
 		
-		ListAdapter adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, valueList);
+		setTitle(contactLoader.getGroupTitle()); 
 		
-		listView.setAdapter(adapter); 
+		contactLoader.execute(); 
 		
 	}
 
@@ -45,6 +64,66 @@ public class DisplayChairContactsActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.display_chair_contacts, menu);
 		return true;
+	}
+	
+	protected class ContactLoader extends AsyncDataLoader<ContactGroup> {
+		private AccessFacade accessFacade; 
+		private ContactLoader(View rootView) {
+			super(rootView);
+		}
+
+		@Override
+		protected ContactGroup doInBackground(Void... unused) {
+			accessFacade = new AccessFacade();
+
+			ContactGroup group = accessFacade.getContactPersonAccess()
+					.getContactGroup(chairId);
+
+//			if (group == null) {
+//				publishProgress(NewsLoader.NO_CONNECTION_PROGRESS);
+//				group = accessFacade.getContactPersonAccess()
+//						.getContactGroupsFromCache();
+//			}
+
+//			if (group == null) {
+//				group = Collections
+//						.unmodifiableList(new ArrayList<ContactGroup>());
+//			}
+
+			return group;
+		}
+
+		@Override
+		protected void onPostExecute(ContactGroup group) {
+			ListView listView = (ListView) rootView.findViewById(R.id.chairContacts);
+
+			
+			
+			final ArrayList<HashMap<String, String>> personList = new ArrayList<HashMap<String, String>>();
+
+			for (ContactPerson person : group.getContactPersons()) {
+				HashMap<String, String> temp1 = new HashMap<String, String>();
+				temp1.put("name", person.getName());
+				temp1.put("office", person.getOffice());
+				temp1.put("telefon", person.getPhone()); 
+				temp1.put("email", person.getEmail()); 
+				temp1.put("description", person.getDescription()); 
+				personList.add(temp1);
+			}
+
+			SimpleAdapter adapter = new SimpleAdapter(rootView.getContext(),
+					personList, R.layout.contact_view,
+					new String[] { "title", "office", "telefon", "email", "description"}, new int[] { R.id.title, R.id.description_contact, R.id.description_contact, R.id.description_contact, R.id.description_contact }
+
+			);
+
+			listView.setAdapter(adapter); 
+		}
+		
+		public String getGroupTitle(){
+			return accessFacade.getContactPersonAccess().getContactGroup(chairId).getTitle(); 
+					
+		}
 	}
 
 }
--- FacultyInfo_App/src/de/uni_passau/facultyinfo/client/fragment/ContactFragment.java
+++ FacultyInfo_App/src/de/uni_passau/facultyinfo/client/fragment/ContactFragment.java
@@ -1,8 +1,11 @@
 package de.uni_passau.facultyinfo.client.fragment;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import android.app.Fragment;
 import android.content.Intent;
@@ -19,11 +22,16 @@ import android.widget.AdapterView.OnItemClickListener;
 import de.uni_passau.facultyinfo.client.R;
 import de.uni_passau.facultyinfo.client.activity.DisplayChairContactsActivity;
 import de.uni_passau.facultyinfo.client.activity.DisplayNewsActivity;
+import de.uni_passau.facultyinfo.client.fragment.NewsFragment.NewsLoader;
+import de.uni_passau.facultyinfo.client.model.access.AccessFacade;
+import de.uni_passau.facultyinfo.client.model.dto.BusLine;
+import de.uni_passau.facultyinfo.client.model.dto.ContactGroup;
 import de.uni_passau.facultyinfo.client.model.dto.News;
+import de.uni_passau.facultyinfo.client.util.AsyncDataLoader;
 
 public class ContactFragment extends Fragment {
-	private View rootView; 
-	
+	private View rootView;
+
 	public ContactFragment() {
 		// Empty constructor required for fragment subclasses
 	}
@@ -31,61 +39,133 @@ public class ContactFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-//		View rootView = inflater.inflate(R.layout.fragment_contacts, container,
-//				false);
-		
+		// View rootView = inflater.inflate(R.layout.fragment_contacts,
+		// container,
+		// false);
+
 		rootView = inflater.inflate(R.layout.fragment_contacts, container,
 				false);
 
 		getActivity().setTitle(R.string.title_contacts);
 
-		ListView listView = (ListView) rootView
-				.findViewById(R.id.contactChairs);
+		// ListView listView = (ListView) rootView
+		// .findViewById(R.id.contactChairs);
+		//
+		// // List valueList = new ArrayList<String>();
+		//
+		// final ArrayList<HashMap<String, String>> chairList = new
+		// ArrayList<HashMap<String, String>>();
+		//
+		// for (int i=0; i<5; i++) {
+		// String iString = ""+i;
+		// HashMap<String, String> temp1 = new HashMap<String, String>();
+		// temp1.put("id", iString);
+		// temp1.put("name", "Lehrstuhl");
+		// chairList.add(temp1);
+		// }
+		//
+		// SimpleAdapter adapter = new SimpleAdapter(rootView.getContext(),
+		// chairList, R.layout.custom_row_view, new String[] { "name",
+		// }, new int[] { R.id.title });
+		//
+		// // for (int i = 0; i < 5; i++) {
+		// // valueList.add("Lehrstuhl " + i);
+		// // }
+		// //
+		// // ListAdapter adapter = new
+		// ArrayAdapter<String>(rootView.getContext(),
+		// android.R.layout.simple_list_item_1, valueList);
+		//
+		// listView.setAdapter(adapter);
 		
-//		List valueList = new ArrayList<String>();
-		
-		final ArrayList<HashMap<String, String>> chairList = new ArrayList<HashMap<String, String>>();
-
-		for (int i=0; i<5; i++) {
-			String iString = ""+i; 
-			HashMap<String, String> temp1 = new HashMap<String, String>();
-			temp1.put("id", iString);
-			temp1.put("name", "Lehrstuhl");
-			chairList.add(temp1);
-		}
-
-		SimpleAdapter adapter = new SimpleAdapter(rootView.getContext(),
-				chairList, R.layout.custom_row_view, new String[] { "name",
-						}, new int[] { R.id.title });
-
-//		for (int i = 0; i < 5; i++) {
-//			valueList.add("Lehrstuhl " + i); 
-//		}
-//		
-//		ListAdapter adapter = new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_list_item_1, valueList);
-		
-		listView.setAdapter(adapter); 
-		
-		listView.setOnItemClickListener(new OnItemClickListener() {
-
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				System.out.println("click");
-				System.out.println(position);
-				displayChairContacts(chairList.get(position).get("id"));
-			}
-		});
+		(new ChairLoader(rootView)).execute(); 
 
 		return rootView;
 
 	}
-	private void displayChairContacts(String id){
+
+	private void displayChairContacts(String id) {
 		Intent intent = new Intent(rootView.getContext(),
 				DisplayChairContactsActivity.class);
 
 		intent.putExtra("chairId", id);
 		System.out.println("putExtra");
 		startActivity(intent);
+	}
+
+	protected class ChairLoader extends AsyncDataLoader<List<ContactGroup>> {
+		private ChairLoader(View rootView) {
+			super(rootView);
+		}
+
+		@Override
+		protected List<ContactGroup> doInBackground(Void... unused) {
+			AccessFacade accessFacade = new AccessFacade();
+
+			List<ContactGroup> groups = accessFacade.getContactPersonAccess()
+					.getContactGroups();
+
+			if (groups == null) {
+				publishProgress(NewsLoader.NO_CONNECTION_PROGRESS);
+				groups = accessFacade.getContactPersonAccess()
+						.getContactGroupsFromCache();
+			}
+
+			if (groups == null) {
+				groups = Collections
+						.unmodifiableList(new ArrayList<ContactGroup>());
+			}
+
+			return groups;
+		}
+
+		@Override
+		protected void onPostExecute(List<ContactGroup> groups) {
+			ListView listView = (ListView) rootView.findViewById(R.id.contactChairs);
+			if(groups.isEmpty()){
+				System.out.println("groups.isEmpty"); 
+			}
+
+			final ArrayList<HashMap<String, String>> groupList = new ArrayList<HashMap<String, String>>();
+
+			for (ContactGroup group : groups) {
+				System.out.println("for");
+				HashMap<String, String> temp1 = new HashMap<String, String>();
+				temp1.put("title", group.getTitle());
+				temp1.put("groupId", group.getId());
+				groupList.add(temp1);
+			}
+
+			SimpleAdapter adapter = new SimpleAdapter(rootView.getContext(),
+					groupList, R.layout.custom_row_view,
+					new String[] { "title"}, new int[] { R.id.title }
+
+			);
+			System.out.println("SimpleAdapter"); 
+			
+			if(adapter.isEmpty()){
+				System.out.println("adapter isEmpty");
+			}
+
+			listView.setAdapter(adapter);
+			
+			System.out.println("setAdapter"); 
+			
+			if(listView.getAdapter().isEmpty()){
+				System.out.println("ListViewAdapter is empty"); 
+			}
+
+			listView.setOnItemClickListener(new OnItemClickListener() {
+
+				@Override
+				public void onItemClick(AdapterView<?> parent, View view,
+						int position, long id) {
+					System.out.println("click");
+					System.out.println(position);
+					displayChairContacts(groupList.get(position).get("groupId"));
+					
+				}
+			});
+		}
 	}
 }
