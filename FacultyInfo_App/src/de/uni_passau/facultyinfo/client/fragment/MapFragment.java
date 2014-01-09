1a7074378d7ee48c42dd8e6ae3ce45652e94fe52
Daniel <Daniel@192.168.0.15>
2014-01-09 22:49:32 +0100

    Marker auf Map funktioniert; Standortbestimmung ebenfalls;

--- FacultyInfo_App/src/de/uni_passau/facultyinfo/client/fragment/MapFragment.java
+++ FacultyInfo_App/src/de/uni_passau/facultyinfo/client/fragment/MapFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import de.uni_passau.facultyinfo.client.R;
 
 public class MapFragment extends Fragment {
@@ -21,18 +27,25 @@ public class MapFragment extends Fragment {
 
 		getActivity().setTitle(R.string.title_activity_map);
 
-		/*
-		 * // Get a handle to the Map Fragment GoogleMap map = ((MapFragment)
-		 * getFragmentManager() .findFragmentById(R.id.map)).getMap();
-		 * 
-		 * LatLng sydney = new LatLng(-33.867, 151.206);
-		 * 
-		 * map.setMyLocationEnabled(true);
-		 * map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
-		 * 
-		 * map.addMarker(new MarkerOptions() .title("Sydney")
-		 * .snippet("The most populous city in Australia.") .position(sydney));
-		 */
+		GoogleMap map = ((com.google.android.gms.maps.MapFragment) getFragmentManager()
+				.findFragmentById(R.id.map)).getMap();
+
+		LatLng sydney = new LatLng(-33.867, 151.206);
+		map.getUiSettings().setCompassEnabled(true);
+		map.getUiSettings().setZoomControlsEnabled(true);
+		map.setMyLocationEnabled(true);
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
+		
+		// Add a marker at San Francisco.
+		map.addMarker(new MarkerOptions().title("San Francisco")
+				.position(new LatLng(37.7750, 122.4183))
+				.snippet("Population: 776733"));
+
+		map.addMarker(new MarkerOptions().title("Sydney")
+				.snippet("The most populous city in Australia.")
+				.position(sydney));
+		// .setVisible(true);
+
 		return rootView;
 
 	}
